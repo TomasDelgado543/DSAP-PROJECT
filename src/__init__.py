59b77c533271852c6d353adc83b7f52ea6c7ed18
TomasDelgado543 <tomas.delgadoayala@unil.ch>
2025-11-24 18:27:28 +0100

    Add package init file to make src importable

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+# src/__init__.py
+"""
+Phillips Curve ML Project - Source Code Package
+
+This package contains modules for:
+- Data collection from FRED API
+- Feature engineering for time series
+- ML model training and evaluation
+- Result visualization
+"""
+
+__version__ = "1.0.0"
+__author__ = "Tomás Delgado"
